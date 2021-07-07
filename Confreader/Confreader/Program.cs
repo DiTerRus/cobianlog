a3a97009257cc109e88deead9446605e69426382
dmitry.terentev <0@dtit.ru>
2021-07-07 15:34:52 +0700

    +w,m,y

--- Confreader/Confreader/Program.cs
+++ Confreader/Confreader/Program.cs
@@ -15,27 +15,37 @@ namespace ConsoleApp3
             string path = @"C:\Program Files (x86)\Cobian Backup 11\DB\MainList.lst";
             // Hashtable logdata = new Hashtable();
             List<string> taskname = new List<string>();
+            List<string> weektaskname = new List<string>();
+            List<string> monthtaskname = new List<string>();
+            List<string> yeartaskname = new List<string>();
             //string filename2 = "log_copy";
             //DateTime dt = new DateTime(1990, 1, 1);
             //string filename = "";
             //new DirectoryInfo(@"C:\1") - путь к папке с архивами
-            
-            
-            
-            
+
+
+
+
 
             //File.Copy(path + filename, path + filename2, true);
 
             string[] readText = File.ReadAllLines(path);
 
             //File.Delete(path + filename2);
-            int n = 0;
+            
             foreach (string confstring in readText.Reverse())
             {
                 if (confstring.Contains("Name="))
                 {
-                    taskname.Add(confstring.Remove(0,5));
-                    n++;
+                    if (confstring.Contains("Name=Week"))
+                        weektaskname.Add(confstring.Remove(0, 5));
+                    if (confstring.Contains("Name=Month"))
+                        monthtaskname.Add(confstring.Remove(0, 5));
+                    if (confstring.Contains("Name=Year"))
+                        yeartaskname.Add(confstring.Remove(0, 5));
+                    else
+                        taskname.Add(confstring.Remove(0,5));
+                    
                 }
                 
             }
@@ -46,6 +56,21 @@ namespace ConsoleApp3
                 message += ("{\"{#TASKNAME}\":\"" + s + "\"},");
             }
 
+            foreach (string s in weektaskname)
+            {
+                message += ("{\"{#WEEKTASKNAME}\":\"" + s + "\"},");
+            }
+
+            foreach (string s in monthtaskname)
+            {
+                message += ("{\"{#MONTHTASKNAME}\":\"" + s + "\"},");
+            }
+
+            foreach (string s in yeartaskname)
+            {
+                message += ("{\"{#YEARTASKNAME}\":\"" + s + "\"},");
+            }
+
             message = message.Trim(',');
             message += "]}";
 
@@ -54,13 +79,13 @@ namespace ConsoleApp3
             
 
 
-            //string message ="The task " + logdata["name"] + " done with " + logdata["errors"] + " errors.";
+           
 
 
 
 
 
-           // Console.ReadLine();
+            Console.ReadLine();
         } //main
     }
 }
